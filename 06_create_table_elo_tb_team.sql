2b7e3a938a416c55e050090db2f564be05135e78
Duane Dicks <34680740+ZombieRunner@users.noreply.github.com>
2023-12-09 09:02:21 +0200

    Create 06_create_table_elo_tb_team.sql

---
+++ 06_create_table_elo_tb_team.sql
@@ -0,0 +1,23 @@
+IF OBJECT_ID('elo.tb_Team') IS NULL
+BEGIN
+	CREATE TABLE elo.tb_Team
+	(
+		ModelID							INT					NOT NULL,
+		TeamID							BIGINT				NOT NULL,
+		TeamName						NVARCHAR(200)		NOT NULL,
+		CONSTRAINT						pk_Team				PRIMARY KEY
+		(
+			ModelID,
+			TeamID
+		),
+		CONSTRAINT						fk_Team_Model		FOREIGN KEY
+		(
+			ModelID
+		)
+		REFERENCES						elo.tb_Model
+		(
+			ModelID
+		)
+	);
+END
+GO;
