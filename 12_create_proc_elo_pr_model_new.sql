9e07d0e24904412556e13169fbea08e74e38b786
Duane Dicks <34680740+ZombieRunner@users.noreply.github.com>
2023-12-09 10:24:17 +0200

    Update 12_create_proc_elo_pr_model_new.sql

--- 12_create_proc_elo_pr_model_new.sql
+++ 12_create_proc_elo_pr_model_new.sql
@@ -1,31 +1,28 @@
-IF OBJECT_ID('elo.pr_Model_New', 'P') IS NULL
+CREATE PROCEDURE elo.pr_Model_New
+(
+	@ModelID					INT,
+	@ModelName					NVARCHAR(50),
+	@ModelDescription			NVARCHAR(1000)	NULL
+)
+AS
+IF (SELECT ISNULL(SUM(ModelID), 0) FROM elo.tb_Model WHERE ModelID = @ModelID) > 0
 BEGIN
-	CREATE PROCEDURE elo.pr_Model_New
-	(
-		@ModelID					INT,
-		@ModelName					NVARCHAR(50),
-		@ModelDescription			NVARCHAR(1000)	NULL
-	)
-	AS
-	IF (SELECT ISNULL(SUM(ModelID), 0) FROM elo.tb_Model WHERE ModelID = @ModelID) > 0
-	BEGIN
-		RAISERROR('ModelID already in use...sorry.', 16, 1);
-		RETURN;
-	END
-	IF (SELECT ISNULL(COUNT(1), 0) FROM elo.tb_Model WHERE ModelName = @ModelName) > 0 
-	BEGIN
-		RAISERROR('ModelName already in use...sorry.', 16, 1);
-		RETURN;
-	END
-	INSERT INTO elo.tb_Model
-	(
-		ModelID,
-		ModelName,
-		ModelDescription 
-	)
-	SELECT
-		@ModelID,
-		@ModelName,
-		@ModelDescription;
+	RAISERROR('ModelID already in use...sorry.', 16, 1);
+	RETURN;
 END
+IF (SELECT ISNULL(COUNT(1), 0) FROM elo.tb_Model WHERE ModelName = @ModelName) > 0 
+BEGIN
+	RAISERROR('ModelName already in use...sorry.', 16, 1);
+	RETURN;
+END
+INSERT INTO elo.tb_Model
+(
+	ModelID,
+	ModelName,
+	ModelDescription 
+)
+SELECT
+	@ModelID,
+	@ModelName,
+	@ModelDescription;
 GO
