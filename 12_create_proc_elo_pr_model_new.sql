6855a5d9fa1dadc25d9e6031b390c2a4804c320b
Duane Dicks <34680740+ZombieRunner@users.noreply.github.com>
2023-12-09 09:36:12 +0200

    Create 12_create_proc_elo_pr_model_new.sql

---
+++ 12_create_proc_elo_pr_model_new.sql
@@ -0,0 +1,31 @@
+IF OBJECT_ID('elo.pr_Model_New', 'P') IS NULL
+BEGIN
+	CREATE PROCEDURE elo.pr_Model_New
+	(
+		@ModelID					INT,
+		@ModelName					NVARCHAR(50),
+		@ModelDescription			NVARCHAR(1000)	NULL
+	)
+	AS
+	IF (SELECT ISNULL(SUM(ModelID), 0) FROM elo.tb_Model WHERE ModelID = @ModelID) > 0
+	BEGIN
+		RAISERROR('ModelID already in use...sorry.', 16, 1);
+		RETURN;
+	END
+	IF (SELECT ISNULL(COUNT(1), 0) FROM elo.tb_Model WHERE ModelName = @ModelName) > 0 
+	BEGIN
+		RAISERROR('ModelName already in use...sorry.', 16, 1);
+		RETURN;
+	END
+	INSERT INTO elo.tb_Model
+	(
+		ModelID,
+		ModelName,
+		ModelDescription 
+	)
+	SELECT
+		@ModelID,
+		@ModelName,
+		@ModelDescription;
+END
+GO
